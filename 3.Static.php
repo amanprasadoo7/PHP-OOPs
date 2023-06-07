ad77944763f1882c09d72a99a8879873369fb281
amanprasadoo7 <amanprasad5455@gmail.com>
2023-06-07 11:44:29 +0530

    Static comments

--- 3.Static.php
+++ 3.Static.php
@@ -5,6 +5,8 @@ class car
 
     public $color;
     private $weight;
+
+    // Static Properties 
     static $model = ["BMW", "AUDI", "Benz", "Lambo"];
     static $year = [1991, 1992, 1993, 1994];
 
@@ -14,18 +16,22 @@ class car
         return $this->color . PHP_EOL;
     }
 
+    // Mutators
     public function setWeight($weight)
     {
         $this->weight = $weight;
     }
 
+    // Accessors
     public function getWeight()
     {
         return $this->weight . PHP_EOL;
     }
 
+    // Static Function
     public static function my_year()
     {
+        // the self keyword is a special keyword that is used to refer to the current class from within the class itself.
         return self::$year;
     }
 }
@@ -38,5 +44,6 @@ echo $myCar->color = "Blue" . PHP_EOL;
 $myCar->setWeight("120Kgs");
 echo $myCar->getWeight() . PHP_EOL;
 
+// calling Static properties and methods.
 print_r(car::$model);
 print_r(car::my_year());
